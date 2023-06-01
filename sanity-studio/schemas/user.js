684dc6f131878421911e15e071f7a06bc5df318a
jjaeseong <jaeseong.park@spresto.net>
2023-06-01 11:17:51 +0900

    set user, post schema

---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,45 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {
+      title: 'Username',
+      name: 'username',
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'eamil',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'username',
+    },
+  },
+}
